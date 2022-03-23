39faa1e9b2e1d61f750637c55876f7e6881b8f26
hrubiaa <hestonrubia01@gmail.com>
2022-03-23 14:07:14 -0700

    pointers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,25 +3,24 @@
 /**
   * print_number - print numbers chars
   * @n: integer params
-  * Return: 0
+  * Return: 
  **/
 
 void print_number(int n)
 {
-	unsigned int n1;
-	
-	n1 = n;
-	
-	if (n < 0)
-	{
-		_putchar('-');
-		n1 = -n;
-	}
+unsigned int n1;
 
-	if (n1 / 10 != 0)
-	{
-		print_number(n1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
+n1 = n;
+
+if (n < 0)
+{
+_putchar('-');
+n1 = -n;
 }
 
+if (n1 / 10 != 0)
+{
+print_number(n1 / 10);
+}
+_putchar((n1 % 10) + '0');
+}
